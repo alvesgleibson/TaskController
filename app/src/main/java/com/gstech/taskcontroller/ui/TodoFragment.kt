e41973a0f8826d6cfc0f1bcf887efc84ba81bf1b
Gleibson Alves <alvesgleibson@gmail.com>
2022-08-17 13:21:07 +0100

    Was added layout NewTask

--- app/src/main/java/com/gstech/taskcontroller/ui/TodoFragment.kt
+++ app/src/main/java/com/gstech/taskcontroller/ui/TodoFragment.kt
@@ -5,16 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.gstech.taskcontroller.R
+import com.gstech.taskcontroller.databinding.FragmentTodoBinding
 
 class TodoFragment : Fragment() {
+
+    private var _binding: FragmentTodoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClicks()
+    }
+
+    private fun initClicks() {
+        newTask()
+    }
+
+    private fun newTask(){
+        binding.fabAddTask.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
